3589f60bf936ff94e1054001aba243c1360d6e88
KentRegs <mkentregalado@gmail.com>
2021-04-06 17:26:38 +0800

    Refactored

--- Session-1/d1a1-master/d1a1-master/account-management/src/main/java/ph/apper/account/App.java
+++ Session-1/d1a1-master/d1a1-master/account-management/src/main/java/ph/apper/account/App.java
@@ -5,10 +5,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class App {
@@ -16,23 +21,53 @@ public class App {
         SpringApplication.run(App.class, args);
     }
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+//    private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
 
-    @RequestMapping("activity")
-    public static class ActivityController {
+    @Bean
+    public RestTemplate restTemplate() { return new RestTemplate(); }
+
+    @RestController
+    @RequestMapping("account")
+    public static class AccountController {
+
+        private final RestTemplate restTemplate;
+
+        public AccountController(RestTemplate restTemplate) {
+            this.restTemplate = restTemplate;
+        }
 
         @PostMapping
-        public ResponseEntity create (@RequestBody Request request) {
+        public ResponseEntity register (@RequestBody CreateAccountRequest request) {
 //            LOGGER.info("{}", request);
             System.out.println(request);
 
+            Activity activity = new Activity();
+            activity.setAction("REGISTRATION");
+            activity.setIdentifier("email="+request.getEmail());
+
+            ResponseEntity<Object> response = restTemplate.postForEntity("http://localhost:8080/activity", activity, Object.class);
+
+            if(response.getStatusCode().is2xxSuccessful()) {
+                System.out.println("Success!");
+            }
+            else {
+                System.out.println("Err = " + response.getStatusCode());
+            }
+
             return ResponseEntity.ok().build();
         }
+    }
+    @Data
+    public static class Activity {
+        private String action;
+        private String identifier;
+    }
 
-        @Data
-        public static class Request {
-            private String action;
-            private String identifier;
-        }
+    @Data
+    public static class CreateAccountRequest {
+        private String firstName;
+        private String lastName;
+        private String email;
+        private String password;
     }
 }
--- Session-1/d1a1-master/d1a1-master/activity/src/main/java/ph/apper/activity/App.java
+++ Session-1/d1a1-master/d1a1-master/activity/src/main/java/ph/apper/activity/App.java
@@ -1,12 +1,12 @@
 package ph.apper.activity;
 
 import lombok.Data;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 public class App {
@@ -14,17 +14,17 @@ public class App {
         SpringApplication.run(App.class, args);
     }
 
-    @RequestMapping("account-management")
-    public static class AccountManagementController {
+//    private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
 
+    @RestController
+    @RequestMapping(path = "activity")
+    public static class ActivityController {
         @PostMapping
-        public ResponseEntity create (@RequestBody Request request) {
-//            LOGGER.info("{}", request);
+        public ResponseEntity create(@RequestBody Request request) {
             System.out.println(request);
 
             return ResponseEntity.ok().build();
         }
-
         @Data
         public static class Request {
             private String action;
