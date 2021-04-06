a6ca8a9c8920bcab1e2178d4c7bdff62a618335a
KentRegs <mkentregalado@gmail.com>
2021-04-06 14:13:26 +0800

    Added postmapping

--- Session-1/d1a1-master/d1a1-master/account-management/src/main/java/ph/apper/account/App.java
+++ Session-1/d1a1-master/d1a1-master/account-management/src/main/java/ph/apper/account/App.java
@@ -1,11 +1,38 @@
 package ph.apper.account;
 
+import lombok.Data;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
+    }
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+
+    @RequestMapping("activity")
+    public static class ActivityController {
+
+        @PostMapping
+        public ResponseEntity create (@RequestBody Request request) {
+//            LOGGER.info("{}", request);
+            System.out.println(request);
+
+            return ResponseEntity.ok().build();
+        }
+
+        @Data
+        public static class Request {
+            private String action;
+            private String identifier;
+        }
     }
 }
--- Session-1/d1a1-master/d1a1-master/activity/src/main/java/ph/apper/activity/App.java
+++ Session-1/d1a1-master/d1a1-master/activity/src/main/java/ph/apper/activity/App.java
@@ -1,11 +1,34 @@
 package ph.apper.activity;
 
+import lombok.Data;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
+    }
+
+    @RequestMapping("account-management")
+    public static class AccountManagementController {
+
+        @PostMapping
+        public ResponseEntity create (@RequestBody Request request) {
+//            LOGGER.info("{}", request);
+            System.out.println(request);
+
+            return ResponseEntity.ok().build();
+        }
+
+        @Data
+        public static class Request {
+            private String action;
+            private String identifier;
+        }
     }
 }
